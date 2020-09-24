b8f20a0eac05c5dd2eb41f2ef3e4f4bdb009b0c3
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-24 10:10:25 -0300

    Create ArvoreBinaria.c

---
+++ files/ArvoreBinaria.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+   int valor;
+   struct node *esq;
+   struct node *dir;
+} node;
+
+void inicializar(node **arvore) {
+   *arvore = NULL;
+}
+
+int arvoreVazia(node *arvore) {
+   return !arvore;
+}
+
+node *novoNo(int valor) {
+   node *novo = (node *) malloc(sizeof(node));
+   if (novo) {
+      novo->valor = valor;
+      novo->esq = novo->dir = NULL;
+   } else printf("\nERRO DE MEMORIA\n");
+   return novo;
+}
+
+node *menorElemento(node *arvore) {
+   if (arvore && arvore->esq)
+      return menorElemento(arvore->esq);
+   else
+      return arvore;
+}
+
+void menorValor(node *arvore) {
+   if (!arvoreVazia(arvore))
+      printf("\nMenor valor: %d", menorElemento(arvore)->valor);
+   else
+      printf("\nArvore vazia!");
+}
+
+node *maiorElemento(node *arvore) {
+   if (arvore && arvore->dir)
+      return maiorElemento(arvore->dir);
+   else
+      return arvore;
+}
+
+void maiorValor(node *arvore) {
+   if (!arvoreVazia(arvore))
+      printf("\nMaior Valor: %d", maiorElemento(arvore)->valor);
+   else
+      printf("\nArvore vazia!");
+}
+
+node *pesquisar(node *arvore, int valor) {
+   if (arvore) {
+      if (arvore->valor == valor)
+         return arvore;
+      if (valor < arvore->valor)
+         return pesquisar(arvore->esq, valor);
+      else
+         return pesquisar(arvore->dir, valor);
+   } else return NULL;
+
+}
+
+void pesquisa(node *arvore, int valor) {
+   printf(pesquisar(arvore, valor) ? "\nEncontrado!" : "\nNao encontrado!");
+}
+
+void *inserir(node **arvore, int valor) {
+   if (arvoreVazia(*arvore))
+      *arvore = novoNo(valor);
+   else if ((*arvore)->valor < valor)
+      inserir(&(*arvore)->dir, valor);
+   else
+      inserir(&(*arvore)->esq, valor);
+}
+
+void remover(node **arvore, int valor) {
+   if (*arvore) {
+      if ((*arvore)->valor == valor) {
+
+         int classif = (*arvore)->esq ? 1 : 0;
+         classif = (*arvore)->dir ? classif ? 3 : 2 : classif;
+
+         if (classif == 0) {
+            free(*arvore);
+            *arvore = NULL;
+         } else if (classif == 1) {
+            node *rmv = *arvore;
+            *arvore = (*arvore)->esq;
+            free(rmv);
+         } else if (classif == 2) {
+            node *rmv = *arvore;
+            *arvore = (*arvore)->dir;
+            free(rmv);
+         } else if (classif == 3) {
+            node *rmv = menorElemento((*arvore)->dir);
+            (*arvore)->esq = rmv->esq;
+            (*arvore)->dir = rmv->dir;
+            free(rmv);
+            remover(&(*arvore)->dir, (*arvore)->valor);
+         }
+
+      }
+      else if (valor < (*arvore)->valor)
+         remover(&(*arvore)->esq, valor);
+      else remover(&(*arvore)->dir, valor);
+   }
+}
+
+void imprimir(node *arvore) {
+   if (arvore) {
+      imprimir(arvore->esq);
+      printf("%d ", arvore->valor);
+      imprimir(arvore->dir);
+   }
+}
+
+void limpar(node *arvore) {
+   if (arvore) {
+      limpar(arvore->esq);
+      limpar(arvore->dir);
+      free(arvore);
+   }
+}
+
+int main(void) {
+   node *arvore;
+   inicializar(&arvore);
+
+   inserir(&arvore, 11);
+   inserir(&arvore, 6);
+   inserir(&arvore, 19);
+   inserir(&arvore, 43);
+   inserir(&arvore, 17);
+   inserir(&arvore, 8);
+   inserir(&arvore, 4);
+   inserir(&arvore, 5);
+   inserir(&arvore, 10);
+   inserir(&arvore, 31);
+   inserir(&arvore, 49);
+
+   imprimir(arvore);
+   menorValor(arvore);
+   maiorValor(arvore);
+
+   pesquisa(arvore, 11);
+   pesquisa(arvore, 6);
+   pesquisa(arvore, 17);
+   pesquisa(arvore, 19);
+   pesquisa(arvore, 8);
+
+   pesquisa(arvore, 3);
+   pesquisa(arvore, 23);
+   pesquisa(arvore, 61);
+
+   limpar(arvore);
+   printf("\n");
+   return 0;
+}
